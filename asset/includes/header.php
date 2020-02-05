63d004ac82eba8d443615bd7632af72a3f6651ae
Vanessa Santana <52542645+vanessa-dev@users.noreply.github.com>
2020-02-04 21:12:57 -0300

    alteracao cadastro php

--- asset/includes/header.php
+++ asset/includes/header.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Etapa 1</title>
-  <link rel="stylesheet" href="asset/css/bootstrap.min.css">
-  <link rel="stylesheet" href="asset/css/dashboard.css">
+  <link rel="stylesheet" href="../asset/css/bootstrap.min.css">
+  <link rel="stylesheet" href="../asset/css/dashboard.css">
   <style>
     .bd-placeholder-img {
       font-size: 1.125rem;
